99dc1b988705581be6d7f19809511a420865a1dc
Bleno Martins Ferreira <blenom.ferreira@gmail.com>
2024-01-25 14:34:12 -0300

    versão final da pagina de projetos

--- Bleno Martins/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('menu-button').addEventListener('click', function() {
-    var menuContent = document.getElementById('menu-content');
-    menuContent.style.display = (menuContent.style.display === 'block' ? 'none' : 'block');
-});
